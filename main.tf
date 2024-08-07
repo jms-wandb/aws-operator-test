4225002ebff7ddec6c21da82f2d2a3dbf8041fc1
Jonathan Stanley <jonathan.stanley@wandb.com>
2024-08-07 15:21:34 -0400

    fixing spacing

--- main.tf
+++ main.tf
@@ -10,12 +10,15 @@ provider "aws" {
     }
   }
 }
+
 data "aws_eks_cluster" "app_cluster" {
   name = module.wandb_infra.cluster_id
 }
+
 data "aws_eks_cluster_auth" "app_cluster" {
   name = module.wandb_infra.cluster_id
 }
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.app_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.app_cluster.certificate_authority[0].data)
@@ -26,6 +29,7 @@ provider "kubernetes" {
     command     = "aws"
   }
 }
+
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.app_cluster.endpoint
@@ -38,9 +42,10 @@ provider "helm" {
     }
   }
 }
+
 module "wandb_infra" {
   source  = "wandb/wandb/aws"
-  version = "4.10.2"
+  version = "4.17.0"
 
   license              = var.wandb_license
   namespace            = var.namespace
@@ -57,7 +62,7 @@ module "wandb_infra" {
   database_sort_buffer_size      = var.database_sort_buffer_size
   allowed_inbound_cidr           = var.allowed_inbound_cidr
   allowed_inbound_ipv6_cidr      = ["::/0"]
-  eks_cluster_version            = "1.25"
+  eks_cluster_version            = "1.28"
   kubernetes_public_access       = true
   kubernetes_public_access_cidrs = ["0.0.0.0/0"]
   domain_name                    = var.domain_name
@@ -76,21 +81,26 @@ module "wandb_infra" {
 output "url" {
   value = module.wandb_infra.url
 }
+
 output "bucket_name" {
   value = module.wandb_infra.bucket_name
 }
+
 output "bucket_queue_name" {
   value = module.wandb_infra.bucket_queue_name
 }
 output "database_instance_type" {
   value = module.wandb_infra.database_instance_type
 }
+
 output "eks_node_instance_type" {
   value = module.wandb_infra.eks_node_instance_type
 }
+
 output "redis_instance_type" {
   value = module.wandb_infra.redis_instance_type
 }
+
 output "standardized_size" {
   value = var.size
 }
